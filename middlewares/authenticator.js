cac3ce930883a8ae9c233b6dbd827f02225b70bc
Hubert LOBIT <hubert.lobit@gmail.com>
2015-10-25 05:09:06 +0100

    Authenticator must setup the user then go next.

--- middlewares/authenticator.js
+++ middlewares/authenticator.js
@@ -5,6 +5,9 @@ module.exports = function getCurrentUser(request, response, next) {
 
   var userHeader = request.headers.user;
 
+  // SET THE HEADER	FOR THE RESPONSE
+  response.set({ 'User': userHeader });
+
   // IF THERE IS NO USER HEADER, YOU CAN'T DO ANYTHING
   if(!userHeader) {
     response.status(401).send('Please log as a user.');
@@ -18,15 +21,16 @@ module.exports = function getCurrentUser(request, response, next) {
     .then(function(user) {
       if(user){
         request.currentUser = user;
+        next();
       } else {
         bookshelf.model('User')
           .forge({ username: userHeader })
-          .save();
+          .save()
+          .then(function(user) {
+            request.currentUser = user;
+            next();
+          });
       }
     });
-
-  // SET THE HEADER	FOR THE RESPONSE
-  response.set({ 'User': userHeader });
-  next();
 
 };
